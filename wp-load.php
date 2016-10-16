999ef5255e519899f3cc1e5f4bbc2ed7c47586e7
DNepovim <jsem@dominikblaha.cz>
2016-10-16 19:30:30 +0200

    Add plugins to composer.

--- wp-load.php
+++ wp-load.php
@@ -18,6 +18,8 @@
  * @package WordPress
  */
 
+require __DIR__ . '/vendor/autoload.php';
+
 /** Define ABSPATH as this file's directory */
 if ( ! defined( 'ABSPATH' ) ) {
 	define( 'ABSPATH', dirname( __FILE__ ) . '/' );
