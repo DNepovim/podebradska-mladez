04e60e25a1b76701aed6ead317ac9de4316e959c
DNepovim <jsem@dominikblaha.cz>
2016-10-18 22:48:50 +0200

    Remove sidebar

--- wp-content/themes/podmld3/functions.php
+++ wp-content/themes/podmld3/functions.php
@@ -18,7 +18,7 @@ function register_my_menus()
 
 // POST THUMBNAILS
 add_theme_support( 'post-thumbnails' );
-add_image_size( 'pozv', 470 );
+add_image_size( 'invitation', 470 );
 
 
 //POST EXCERPT MORE
@@ -26,15 +26,3 @@ function new_excerpt_more( $more ) {
     return ' <a class="read-more" href="'. get_permalink( get_the_ID() ) . '">' . __('Pokračovat ve čtení...', 'your-text-domain') . '</a>';
 }
 add_filter( 'excerpt_more', 'new_excerpt_more' );
-
-//SIDEBAR
-if ( function_exists('register_sidebar') ) {
-    register_sidebar(array(
-        'name' => 'Main Sidebar',
-        'before_widget' => '<div id="%1$s" class="widget %2$s">',
-        'after_widget' => '</div>',
-        'before_title' => '<h3 class="widget-title">',
-        'after_title' => '</h3>',
-    ));
-}
-?>
