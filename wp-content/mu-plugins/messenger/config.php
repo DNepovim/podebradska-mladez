59275368a59a66bc7593abdc7a7f3d2910c82a18
DNepovim <jsem@dominikblaha.cz>
2016-10-20 04:25:46 +0200

    Remove config from git

--- wp-content/mu-plugins/messenger/config.php
+++
@@ -1,4 +0,0 @@
-<?php
-$access_token     = "EAAFmHXVGchYBAH3rtQ3IOeJSKL0TXXCIUnD4WHrLMN7GscxuW8VkVszCwuijcP97H0Uz7Fqo28UkbUWiR00inu9pBJrZAoQfNwwZAkqhChdB3QXTxZCv7ZBzkZCCJRoLWURQoPEkJ78cS28utBglJ0nigFzoSdi2ZB1c2ljOkEZBAZDZD";
-$verify_token     = "call_me_something";
-$hub_verify_token = null;
