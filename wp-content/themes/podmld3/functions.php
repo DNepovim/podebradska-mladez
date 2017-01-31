59627c60f71d224e46201367a8cafd140a9ced6e
DNepovim <jsem@dominikblaha.cz>
2017-01-31 23:59:56 +0100

    Show hashstags in events archive

--- wp-content/themes/podmld3/functions.php
+++ wp-content/themes/podmld3/functions.php
@@ -26,3 +26,46 @@ function new_excerpt_more( $more ) {
     return ' <a class="read-more" href="'. get_permalink( get_the_ID() ) . '">' . __('Pokračovat ve čtení...', 'your-text-domain') . '</a>';
 }
 add_filter( 'excerpt_more', 'new_excerpt_more' );
+
+function get_event_fb_posts($postID){
+
+	$hashtag = get_post_meta($postID, 'pm_hashtag', true);
+
+	if (!empty($hashtag)) {
+
+		$args = array(
+			'post_type'  => 'fb',
+			'meta_key'   => 'fptc_hashtag',
+			'meta_value' => $hashtag
+		);
+
+		$fbPosts = get_posts($args);
+
+		return $fbPosts;
+
+	} else {
+		return false;
+	}
+}
+
+function get_event_hashtags($postID) {
+
+	$fbPosts = get_event_fb_posts($postID);
+
+	if (!empty($fbPosts)) {
+
+		$hashtags = array();
+
+		foreach ($fbPosts as $post) {
+
+			$potsHashtags = get_post_meta($post->ID, 'fptc_hashtag');
+
+			$hashtags = array_merge($hashtags, $potsHashtags);
+		}
+
+		return array_unique($hashtags);
+
+	} else {
+		return false;
+	}
+}
