edba919f887f632fd13b3dd642c920db991bf9b4
DNepovim <jsem@dominikblaha.cz>
2017-09-11 00:48:55 +0200

    404 page

--- theme/404.php
+++ theme/404.php
@@ -1,4 +1,16 @@
 <?php
+$args['nextEvent'] = get_posts([
+	'post_type'  => 'events',
+	'numberposts'     => -1,
+	'order' => 'ASC',
+	'meta_query' => [
+		[
+			'key'     => 'pm_end_date',
+			'value'   => current_time( 'Y-m-d H:i' ),
+			'compare' => '>'
+		]
+	]
+])[0];
 
 // latte file has same name as this file
-view(basename(__FILE__ , '.php'));
+view($args);
