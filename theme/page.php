913be0ca55490a0675806fa9e1ec9ff3bc820f37
DNepovim <jsem@dominikblaha.cz>
2017-09-11 00:34:43 +0200

    Page template

--- theme/page.php
+++ theme/page.php
@@ -1,4 +1,12 @@
 <?php
 
+$post_type = meta(get_the_ID(), 'pm_page_posts');
+
+$args['posts'] = get_posts([
+	'post_type'  => $post_type,
+	'numberposts' => -1,
+	'order' => 'DESC'
+]);
+
 // latte file has same name as this file
-view();
+view($args);
