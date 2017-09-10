8315c0753625fa113a412f41964eb8c1c71eb444
DNepovim <jsem@dominikblaha.cz>
2017-09-10 22:05:04 +0200

    Image sizes

--- theme/utils/image-sizes.php
+++ theme/utils/image-sizes.php
@@ -2,11 +2,6 @@
 
 add_action('after_setup_theme', 'add_image_sizes');
 function add_image_sizes() {
-	add_image_size('xl', 1920);
-	add_image_size('l', 1280);
-	add_image_size('m', 960);
-	add_image_size('s', 500);
-	add_image_size('xs', 300);
-	add_image_size('xxs', 100);
-	add_image_size('xxxs', 50);
+	add_image_size('invitation', 500);
+	add_image_size('wrapper', 300);
 }
