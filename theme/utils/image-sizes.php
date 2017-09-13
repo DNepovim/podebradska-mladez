381a912f1e68590461be4dd097686d7049cbcee7
DNepovim <jsem@dominikblaha.cz>
2017-09-13 15:56:03 +0200

    Style page article

--- theme/utils/image-sizes.php
+++ theme/utils/image-sizes.php
@@ -4,5 +4,5 @@ add_action('after_setup_theme', 'add_image_sizes');
 function add_image_sizes() {
 	add_image_size('invitation', 500);
 	add_image_size('wrapper', 300);
-	add_image_size('person', 300, 300, true);
+	add_image_size('person', 450, 450, true);
 }
