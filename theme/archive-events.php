646636e776b2cdc86053a96ad8269263b5935ef2
DNepovim <jsem@dominikblaha.cz>
2017-11-21 22:26:50 +0100

    Fix archive events sorting

--- theme/archive-events.php
+++ theme/archive-events.php
@@ -6,6 +6,8 @@ $args['events'] = get_posts([
 	'post_type' => 'events',
 	'numberposts' => -1,
 	'order' => 'DESC',
+	'orderby' => 'meta_value',
+	'meta_key' => 'pm_start_date',
 	'meta_query' => [
 		[
 			'key'     => 'pm_end_date',
