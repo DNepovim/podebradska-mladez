e04757e76f125d44d51373622833c63cd22c01a8
DNepovim <jsem@dominikblaha.cz>
2017-11-24 11:51:40 +0100

    Fix registration form empty state

--- theme/front-page.php
+++ theme/front-page.php
@@ -29,7 +29,13 @@ $futureEvents = array_slice($events, 1);
 
 $map = $nextEvent ? get_post_meta(meta($nextEvent->ID,'pm_position'), 'pm_position', true) : false;
 
-$registerTo = $nextEvent && meta($nextEvent->ID, 'pm_register_to') >= current_time('Y-m-d') ? meta($nextEvent->ID, 'pm_register_to') : false;
+if ($registerTo = meta($nextEvent->ID, 'pm_register_to')) {
+	if ($registerTo >= current_time('Y-m-d')) {
+		$registerTo = -1;
+	}
+} else {
+	$registerTo = -1;
+}
 
 // latte file has same name as this file
 view(['nextEvent' => $nextEvent, 'map' => $map, 'futureEvents' => $futureEvents, 'registerTo' => $registerTo ]);
