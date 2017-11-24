4b43cbd03821d1b19c96f5f3b3ac0be9d6f8c593
DNepovim <jsem@dominikblaha.cz>
2017-11-24 14:17:08 +0100

    Redesign gallery and add to front page

--- theme/front-page.php
+++ theme/front-page.php
@@ -5,8 +5,7 @@ if(isset($_GET['styleguide'])) {
 	exit;
 }
 
-
-$args = [
+$events = get_posts([
 	'post_type'  => 'events',
 	'numberposts'     => -1,
 	'order' => 'ASC',
@@ -19,15 +18,13 @@ $args = [
 			'compare' => '>',
 		]
 	]
-];
+]);
 
-$events = get_posts($args);
+$args['nextEvent'] = $events ? $events[0] : false;
 
-$nextEvent = $events ? $events[0] : false;
+$args['futureEvents'] = array_slice($events, 1);
 
-$futureEvents = array_slice($events, 1);
-
-$map = $nextEvent ? get_post_meta(meta($nextEvent->ID,'pm_position'), 'pm_position', true) : false;
+$args['map'] = $nextEvent ? get_post_meta(meta($nextEvent->ID,'pm_position'), 'pm_position', true) : false;
 
 if ($registerTo = meta($nextEvent->ID, 'pm_register_to')) {
 	if ($registerTo >= current_time('Y-m-d')) {
@@ -37,5 +34,26 @@ if ($registerTo = meta($nextEvent->ID, 'pm_register_to')) {
 	$registerTo = -1;
 }
 
-// latte file has same name as this file
-view(['nextEvent' => $nextEvent, 'map' => $map, 'futureEvents' => $futureEvents, 'registerTo' => $registerTo ]);
+$args['registerTo'] = $registerTo;
+
+if (!has_post_thumbnail($nextEvent->ID)) {
+	$args['lastEvent'] = get_posts([
+		'post_type'  => 'events',
+		'numberposts'     => 1,
+		'order' => 'DESC',
+		'orderby' => 'meta_value',
+		'meta_key' => 'pm_start_date',
+		'meta_query' => [
+			[
+				'key'     => 'pm_end_date',
+				'value'   => current_time( 'Y-m-d H:i' ),
+				'compare' => '<',
+			]
+		]
+	])[0];
+
+	$args['lastEventPhotos'] = meta($args['lastEvent']->ID, 'pm_google_photos');
+}
+
+
+view($args);
