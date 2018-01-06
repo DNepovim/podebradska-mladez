993763aabb9f6ddd6e21efe21a4af841fa4f05a6
DNepovim <jsem@dominikblaha.cz>
2018-01-06 21:25:41 +0100

    2048

---
+++ theme/template-2048.php
@@ -0,0 +1,4 @@
+<?php
+/* Template Name: 2048 */
+
+view();
--- theme/utils/image-sizes.php
+++ theme/utils/image-sizes.php
@@ -5,4 +5,5 @@ function add_image_sizes() {
 	add_image_size('invitation', 500);
 	add_image_size('wrapper', 300);
 	add_image_size('person', 450, 450, true);
+	add_image_size('tile', 150, 150, true);
 }
