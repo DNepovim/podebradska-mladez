94fc67295db07e909661ed3cac1a15d1ee1eb553
DNepovim <jsem@dominikblaha.cz>
2018-01-06 22:08:00 +0100

    SEO

--- theme/utils/image-sizes.php
+++ theme/utils/image-sizes.php
@@ -6,4 +6,5 @@ function add_image_sizes() {
 	add_image_size('wrapper', 300);
 	add_image_size('person', 450, 450, true);
 	add_image_size('tile', 150, 150, true);
+	add_image_size('graph', 1200, 630, true);
 }
--- theme/utils/site_title.php
+++ theme/utils/site_title.php
@@ -4,18 +4,20 @@ use Nette\Utils\Html;
 function site_title() {
 	$title = Html::el();
 
+	$site_name = get_bloginfo('name') . ' ' . get_bloginfo('description');
+
 	if (function_exists('is_tag') && is_tag()) {
-		$title->setHtml('Tag Archive for "' . $tag . '" - ' . get_bloginfo('name'));
+		$title->setHtml('Tag Archive for "' . $tag . '" | ' . $site_name);
 	} elseif (is_archive()) {
-		$title->setHtml(wp_title('', FALSE) . ' Archive - ' . get_bloginfo('name'));
+		$title->setHtml(wp_title('', FALSE) . ' Archive | ' . $site_name);
 	} elseif (is_search()) {
-		$title->setHtml('Search for "' . wp_specialchars($s) . '" - ' . get_bloginfo('name'));
+		$title->setHtml('Search for "' . wp_specialchars($s) . '" | ' . $site_name);
 	} elseif (!(is_404()) && (is_single()) || (is_page())) {
-		$title->setHtml(wp_title('', FALSE) . ' - ' . get_bloginfo('name'));
+		$title->setHtml(wp_title('', FALSE) . ' | ' . $site_name);
 	} elseif (is_404()) {
-		$title->setHtml('Not Found - ' . get_bloginfo('name'));
+		$title->setHtml('Not Found | ' . $site_name);
 	} else {
-		$title->setHtml(get_bloginfo('name'));
+		$title->setHtml($site_name);
 	}
 
 	return $title;
