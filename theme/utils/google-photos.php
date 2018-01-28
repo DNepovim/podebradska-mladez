9f18b76930f7ec4f42e82ab326697bd6729c46aa
DNepovim <jsem@dominikblaha.cz>
2018-01-28 21:45:13 +0100

    Dont create new google photos meta if already exists

--- theme/utils/google-photos.php
+++ theme/utils/google-photos.php
@@ -3,15 +3,23 @@
 
 function load_google_photos($post_id) {
 	$url = meta($post_id, 'pm_google_photos_album');
+	if (!empty($url)) {
+		$content = file_get_contents($url);
+		preg_match('/AF_initDataCallback(.*)function\(\){return ([^%]*)/', $content, $match);
+		$next = substr($match[0], strpos($match[0], '{return ') + 8);
+		$last = substr($next, 0, strpos($next, '}});'));
+		$json = json_decode($last);
+		$images = [];
+		foreach ($json[1] as $item) {
+			$images[] = $item[1][0];
+		}
 
-	$content = file_get_contents($url);
-	preg_match('/AF_initDataCallback(.*)function\(\){return ([^%]*)/', $content, $match);
-	$next = substr($match[0], strpos($match[0], '{return ') + 8);
-	$last = substr($next, 0, strpos($next, '}});'));
-	$json = json_decode($last);
-	$images = [];
-	foreach ($json[1] as $item) {
-		$images[] = $item[1][0];
+		if (metadata_exists('post', $post_id, 'pm_google_photos')) {
+			return update_post_meta($post_id, 'pm_google_photos', $images);
+		} else {
+			return add_post_meta($post_id, 'pm_google_photos', $images);
+		}
+	} else {
+		return delete_post_meta($post_id, 'pm_google_photos');
 	}
-	return add_post_meta($post_id, 'pm_google_photos', $images);
 }
