fe1c2a3d459214f79c64608d01762cc285b9f53d
Dominik Bláha <jsem@dominikblaha.cz>
2020-05-20 22:52:22 +0200

    Fix site title

--- theme/utils/site_title.php
+++ theme/utils/site_title.php
@@ -6,18 +6,10 @@ function site_title() {
 
 	$site_name = get_bloginfo('name') . ' ' . get_bloginfo('description');
 
-	if (function_exists('is_tag') && is_tag()) {
-		$title->setHtml('Tag Archive for "' . $tag . '" | ' . $site_name);
-	} elseif (is_archive()) {
-		$title->setHtml(wp_title('', FALSE) . ' Archive | ' . $site_name);
-	} elseif (is_search()) {
-		$title->setHtml('Search for "' . wp_specialchars($s) . '" | ' . $site_name);
-	} elseif (!(is_404()) && (is_single()) || (is_page())) {
-		$title->setHtml(wp_title('', FALSE) . ' | ' . $site_name);
-	} elseif (is_404()) {
-		$title->setHtml('Not Found | ' . $site_name);
-	} else {
+	if (is_front_page()) {
 		$title->setHtml($site_name);
+	} else {
+		$title->setHtml(wp_title('', FALSE) . ' | ' . $site_name);
 	}
 
 	return $title;
